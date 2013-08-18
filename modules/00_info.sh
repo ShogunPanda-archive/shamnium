c71f190d48f4b3e4dbec22366fc86015cd1df386
ShogunPanda <shogun_panda@me.com>
2013-08-18 16:35:29 -0700

    Update email.

--- modules/00_info.sh
+++ modules/00_info.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
-# This file is part of shamnium. Copyright (C) 2013 and above Shogun <shogun_panda@cowtech.it>.
+# This file is part of shamnium. Copyright (C) 2013 and above Shogun <shogun@cowtech.it>.
 # Licensed under the MIT license, which can be found at http://www.opensource.org/licenses/mit-license.php.
 #
 
-export SHAMNIUM_VERSION="2.2.0"
+export SHAMNIUM_VERSION="2.2.1"
